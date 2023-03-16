c61d4ad69b9010c417b7a0c8e5f2b60c48a7c016
mygoaway <mygoaway2@gmail.com>
2023-03-16 12:24:46 +0900

    데이터 접근 기술 - Querydsl 강의 후 커밋

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
--- src/main/java/hello/itemservice/service/ItemServiceV1.java
+++ src/main/java/hello/itemservice/service/ItemServiceV1.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 @RequiredArgsConstructor
 public class ItemServiceV1 implements ItemService {
 
